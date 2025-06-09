0a1231b0b1b134982e10bed41f2395102fb99daa
jdy8739 <jdy8739@naver.com>
2025-06-10 01:11:37 +0900

    Banner and car responsive markup

--- src/components/blocks/Banner.tsx
+++ src/components/blocks/Banner.tsx
@@ -10,20 +10,25 @@ const Banner = () => {
       py-[6.25rem_6.81rem]
       px-[2.00rem]
       flex flex-col gap-[2.50rem]
-      "
+      md:px-[4.50rem]
+      md:flex-row"
     >
-      <p className="text-[1.19rem] tracking-wide font-bold text-white">OVER 4000 LATEST CARS</p>
-      <p className="text-[2.63rem] font-semibold text-white leading-[1.1]">
-        Discover The Ease Of
-        <span className="text-[#DD2F30]"> Getting Your Dream </span>
-        Car With Quality
-      </p>
-      <p className="text-[#A6A6A6] text-[1.19rem]">
-        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat
-        facere.
-      </p>
-      <div>
-        <BorderButton>MORE INFORMATION</BorderButton>
+      <div className="flex flex-col gap-[2.50rem]">
+        <p className="text-[1.19rem] tracking-wide font-bold text-white">OVER 4000 LATEST CARS</p>
+        <p className="text-[2.63rem] font-semibold text-white leading-[1.1]">
+          Discover The Ease Of
+          <span className="text-[#DD2F30]"> Getting Your Dream </span>
+          Car With Quality
+        </p>
+      </div>
+      <div className="flex flex-col gap-[2.50rem] md:pt-[4.00rem]">
+        <p className="text-[#A6A6A6] text-[1.19rem]">
+          Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+          facere.
+        </p>
+        <div>
+          <BorderButton>MORE INFORMATION</BorderButton>
+        </div>
       </div>
     </section>
   );
--- src/components/blocks/Car.tsx
+++ src/components/blocks/Car.tsx
@@ -13,33 +13,58 @@ const Car = () => {
       relative
       flex
       flex-col
-      gap-[1.88rem]"
+      gap-[1.88rem]
+      md:px-[4.50rem]
+      "
     >
-      <div className="absolute top-0 left-0 text-[15.75rem] font-bold text-[#FEFEFE] leading-[1.1] opacity-8">
+      <div
+        className="
+        absolute
+        top-0
+        left-0
+        text-[15.75rem]
+        font-bold
+        text-[#FEFEFE]
+        leading-[1.1]
+        opacity-8
+        md:text-[21.25rem]"
+      >
         DEALER
       </div>
-      <ul className="flex flex-col gap-[1.88rem]">
-        <li className="flex flex-col gap-[0.38rem]">
-          <div className="text-[#CC3333] text-[2.63rem] leading-[1.1] font-medium">900+</div>
-          <div className="text-[#FEFEFE] text-[1.19rem]">Happy Clients</div>
-        </li>
-        <hr className="text-[4.75rem] text-[#A6A6A6] w-[4.75rem]" />
-        <li className="flex flex-col gap-[0.38rem]">
-          <div className="text-[#CC3333] text-[2.63rem] leading-[1.1] font-medium">500+</div>
-          <div className="text-[#FEFEFE] text-[1.19rem]">Available Cars</div>
-        </li>
-        <hr className="text-[4.75rem] text-[#A6A6A6] w-[4.75rem]" />
-        <li className="flex flex-col gap-[0.38rem]">
-          <div className="text-[#CC3333] text-[2.63rem] leading-[1.1] font-medium">30+</div>
-          <div className="text-[#FEFEFE] text-[1.19rem]">Years Experience</div>
-        </li>
-      </ul>
-      <p className="text-[#A6A6A6] text-[0.94rem] leading-[1.6]">
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-        aute irure dolor in velit.
-      </p>
-      <div className="relative h-[21.31rem]">
-        <img src={car} alt="car" className="absolute bottom-0 left-0 max-w-none h-full" />
+      <div className="flex flex-col gap-[6.25rem] md:flex-col-reverse">
+        <div>
+          <ul className="flex flex-col gap-[1.88rem] md:flex-row">
+            <li className="flex flex-col gap-[0.38rem]">
+              <div className="text-[#CC3333] text-[2.63rem] leading-[1.1] font-medium">900+</div>
+              <div className="text-[#FEFEFE] text-[1.19rem]">Happy Clients</div>
+            </li>
+            <div className="w-[4.75rem] h-[1px] bg-[#A6A6A6] md:w-[0.06rem] md:h-20" />
+            <li className="flex flex-col gap-[0.38rem]">
+              <div className="text-[#CC3333] text-[2.63rem] leading-[1.1] font-medium">500+</div>
+              <div className="text-[#FEFEFE] text-[1.19rem]">Available Cars</div>
+            </li>
+            <div className="w-[4.75rem] h-[1px] bg-[#A6A6A6] md:w-[0.06rem] md:h-20" />
+            <li className="flex flex-col gap-[0.38rem]">
+              <div className="text-[#CC3333] text-[2.63rem] leading-[1.1] font-medium">30+</div>
+              <div className="text-[#FEFEFE] text-[1.19rem]">Years Experience</div>
+            </li>
+          </ul>
+          <p className="text-[#A6A6A6] text-[0.94rem] leading-[1.6]">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in velit.
+          </p>
+          <div className="relative h-[21.31rem] md:h-[25.88rem]">
+            <img
+              src={car}
+              alt="car"
+              className="absolute bottom-0 left-0 md:right-[-12rem] md:left-auto max-w-none h-full"
+            />
+          </div>
+        </div>
+        <div>
+          <p className="font-bold tracking-wider text-white text-[0.94rem]">GET TO KNOW MORE:</p>
+          <ul></ul>
+        </div>
       </div>
     </section>
   );
